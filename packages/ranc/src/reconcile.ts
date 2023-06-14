824d8f6f0db72fa211e1f04f0978169e29affe4a
chaxus <nouo18@163.com>
2023-06-14 13:50:49 +0800

    Feat/type (#4)

--- packages/ranc/src/reconcile.ts
+++ packages/ranc/src/reconcile.ts
@@ -1,4 +1,4 @@
-import { TAG } from '@/src/type'
+import { HostRoot , TAG } from '@/src/type'
 import type {
   DOMElement,
   Effect,
@@ -13,28 +13,42 @@ import { commit } from '@/src/commit'
 import { initArray } from '@/src/utils'
 import type { VNode } from '@/src/vdom'
 
+interface RootNode {
+  node: Element
+  props: {
+    children: Array<VNode>
+  }
+}
+
+const ROOT_NODE = 'root'
 
 /**
- * @description: reconcile.ts 负责 vdom 转 fiber 
+ * @description: reconcile.ts 负责 vdom 转 fiber
  */
 let currentFiber: Fiber
 
-export const render = (vnode: VNode, node: DOMElement | null): void => {
+export const render = (vnode: VNode, node: Element): void => {
   if (vnode && node) {
     const root = {
       node,
-      props: { children: vnode },
-    } as any
+      props: { children: [vnode] },
+    }
     update(root)
   }
 }
 
-export const update = (fiber: Fiber<FiberProps> | undefined): void => {
-  if (fiber && !fiber?.dirty) {
-    // 标记为 dirty 表示更新过了
-    fiber.dirty = true
-    schedule(() => reconcile(fiber))
+export const update = (root: RootNode): void => {
+  const { node, props } = root
+  const fiber: Fiber = {
+    tag: HostRoot,
+    type: ROOT_NODE,
+    node,
+    props,
+    dirty: true,
+    lane: TAG.UPDATE,
+    isComp: false,
   }
+  schedule(() => reconcile(fiber))
 }
 
 const reconcile = (fiber?: Fiber): boolean => {
@@ -111,7 +125,7 @@ const updateHook = (fiber: Fiber): void => {
   }
 }
 
-const updateHost = (fiber: Fiber): void => {
+const  updateHost = (fiber: Fiber): void => {
   fiber.parentNode = getParentNode(fiber)
   if (!fiber.node) {
     const flag = createElement(fiber)
@@ -123,6 +137,7 @@ const updateHost = (fiber: Fiber): void => {
 }
 
 const getParentNode = (fiber: Fiber): DOMElement | undefined => {
+  if (!fiber.isComp) return fiber.node
   while (fiber.parent && (fiber = fiber.parent)) {
     if (!fiber.isComp) return fiber.node
   }
